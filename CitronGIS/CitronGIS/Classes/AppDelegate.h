b349b1b93c7844d4d1ded9db46b2bcf60b3a4fe1
Charly DELAROCHE <charlydelaroche@pc6.home>
2014-05-05 20:10:52 +0200

    initial commit

---
+++ CitronGIS/CitronGIS/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CitronGIS
+//
+//  Created by Charly DELAROCHE on 05/05/14.
+//  Copyright Charly DELAROCHE 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CitronGIS/CitronGIS/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CitronGIS
+//
+//  Created by Charly DELAROCHE on 05/05/14.
+//  Copyright Charly DELAROCHE 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}

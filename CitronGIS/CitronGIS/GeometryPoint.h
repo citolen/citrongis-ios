e02bb02365bd8e2e882e6fb1292bac8ee22df3dc
Charly DELAROCHE <charlydelaroche@pc6.home>
2014-05-12 21:25:21 +0200

    geometryPoint

---
+++ CitronGIS/CitronGIS/GeometryPoint.h
@@ -0,0 +1,14 @@
+//
+//  GeometryPoint.h
+//  CitronGIS
+//
+//  Created by Charly DELAROCHE on 12/05/14.
+//  Copyright (c) 2014 Charly DELAROCHE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GeometryPoint : NSObject <NSCoding>
+
+
+@end
---
+++ CitronGIS/CitronGIS/GeometryPoint.m
@@ -0,0 +1,13 @@
+//
+//  GeometryPoint.m
+//  CitronGIS
+//
+//  Created by Charly DELAROCHE on 12/05/14.
+//  Copyright (c) 2014 Charly DELAROCHE. All rights reserved.
+//
+
+#import "GeometryPoint.h"
+
+@implementation GeometryPoint
+
+@end

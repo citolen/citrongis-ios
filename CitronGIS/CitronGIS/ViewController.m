0f3749d09c6df18fd8d0c343e3eff220496f6831
Charly DELAROCHE <charlydelaroche@pc6.home>
2014-05-12 21:16:27 +0200

    base proj

--- CitronGIS/CitronGIS/ViewController.m
+++ CitronGIS/CitronGIS/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "SVProgressHUD.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +19,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    [SVProgressHUD showWithStatus:@"HI BITCH.............................. endless progress"];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 

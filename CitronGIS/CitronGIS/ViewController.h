778a0c40293a4274e4c3fe9266f0b42c148e34ea
Charly DELAROCHE <charlydelaroche@charlys-mbp.home>
2014-05-20 09:49:42 +0200

    PullableView

--- CitronGIS/CitronGIS/ViewController.h
+++ CitronGIS/CitronGIS/ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PullableView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <PullableViewDelegate>
+
+
 
 @end
